6f3fa2e16e8760b20a2497ff536a3ab3389c8470
Maksim <m.v.sharipov@gmail.com>
2019-02-28 19:22:18 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# useEnhancedReducer [![npm version](https://img.shields.io/npm/v/use-enhanced-reducer.svg?style=flat)](https://www.npmjs.com/package/use-enhanced-reducer)
+
+Custom hook for use React.useReducer with middlewares.
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+export type TReducerMiddleware<S = any, A = any> = (
+	state: S,
+	reducer: React.Reducer<S, A>,
+) => (dispatch: React.Dispatch<A>) => (action: A) => void;
+
+export function useEnhancedReducer<S, A>(
+	reducer: React.Reducer<S, A>,
+	initalState: S,
+	middlewares: TReducerMiddleware[] = [loggerMiddleware],
+): [S, React.Dispatch<A>] {
+	const [state, dispatch] = React.useReducer(reducer, initalState);
+
+	if (!Array.isArray(middlewares) || middlewares.length === 0) {
+		return [state, dispatch];
+	}
+
+	const chain = middlewares.map(middleware => middleware(state, reducer));
+	const enhancedDispatch = compose(...chain)(dispatch);
+
+	return [state, enhancedDispatch];
+}
+
+export const loggerMiddleware: TReducerMiddleware = (state, reducer) => dispatch => action => {
+	console.info('%cPrevious State:', 'color: #9E9E9E; font-weight: 700;', state);
+	console.info('%cAction:', 'color: #00A7F7; font-weight: 700;', action);
+	console.log('%cNext State:', 'color: #47B04B; font-weight: 700;', reducer(state, action));
+
+	dispatch(action);
+};
+
+export const thunkMiddleware: TReducerMiddleware = state => dispatch => action => {
+	if (typeof action === 'function') {
+		action(dispatch, state);
+		return;
+	}
+
+	dispatch(action);
+};
+
+const compose = <V>(...fns: Array<(value: V) => V>) => (value: V) => fns.reduceRight((prevValue, fn) => fn(prevValue), value);

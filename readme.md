fc3eae2f69778d2fab39d8a6500e2a57d68ef2a7
Maksim <m.v.sharipov@gmail.com>
2019-03-29 17:11:01 +0200

    Rollup added

--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 [![npm version](https://badgen.net/npm/v/use-enhanced-reducer?icon=npm)](https://www.npmjs.com/package/use-enhanced-reducer)
 [![npm downloads](https://badgen.net/npm/dt/use-enhanced-reducer?icon=libraries&color=green)](https://www.npmjs.com/package/use-enhanced-reducer)
+[![bundle size](https://badgen.net/bundlephobia/minzip/use-enhanced-reducer@latest?icon=awesome)](https://bundlephobia.com/result?p=use-enhanced-reducer@latest)
 
 Simple custom hook for use React.useReducer with middlewares.
 
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: "./dist/index.js",
+      format: "cjs"
+    },
+    {
+      file: "./dist/index.es.js",
+      format: "es"
+    }
+  ],
+  external: ["react"],
+  plugins: [
+    typescript(),
+    terser({
+      toplevel: true,
+      compress: {
+        passes: 3,
+        pure_getters: true,
+        unsafe: true
+      }
+    })
+  ]
+};
